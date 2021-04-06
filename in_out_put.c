9cf627f9ba3913e51c66961f512d7af1b3c156ce
gustavohubner <34004020+gustavohubner@users.noreply.github.com>
2021-04-05 22:17:10 -0300

    eu fiz algo

--- in_out_put.c
+++ in_out_put.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int substring(char* string, size_t* len, char* sub);
+int substring(char *string, size_t *len, char *sub);
 
 int main(int argc, char **argv)
 {
-	char file_name[30] = "input/input.txt";
+	if (argc < 2)
+	{
+		printf("Insira uma entrada\n");
+		exit(0);
+	}
 	char to_comp[21];
-	char* line = NULL;
+	char *line = NULL;
 	size_t len = 0;
 	size_t read;
-	FILE* file; 
+	FILE *file;
 	int oc_count = 0;
-	
-	if ((file = fopen(file_name, "r"))){   
+
+	if ((file = fopen(argv[1], "r")))
+	{
 		int i;
 		char c;
-		   
-		for(i = 0; (c = getc(file)) != '\n' && c != '\r' && c != ' '; i++){
+
+		for (i = 0; (c = getc(file)) != '\n' && c != '\r' && c != ' '; i++)
+		{
 			to_comp[i] = c;
 		}
 		to_comp[i] = 0;
 
-		while ((read = getline(&line, &len, file)) != -1) {				
-			oc_count += substring(line, &read, to_comp); 
-		}		
-		
+		while ((read = getline(&line, &len, file)) != -1)
+		{
+			oc_count += substring(line, &read, to_comp);
+		}
+
 		printf("Total de ocorrencias: %d\n", oc_count);
-	} else {
+	}
+	else
+	{
 		printf("Oh, godddamnit");
 	}
-	
+
 	fclose(file);
 	return 0;
 }
-
